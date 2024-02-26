f8fb5c71eadbeda53b23da5cd2156e3a8d0bb6a3
kheira <kheira.Je29@gmail.com>
2024-02-26 09:17:14 -0800

    Updated the prompt message for location input

---
+++ user-input.py
@@ -0,0 +1,12 @@
+# Ask for user's name 
+name = input("What is your name?")
+# Ask for user's age
+age = input("How old are you?")
+# Ask for user's location
+location = input("Where are you located?")
+# print personalized message 
+print("Hello {},your are {} years old and live in {}.".format(name, age, location))
+
+
+
+
